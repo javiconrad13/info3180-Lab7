7959b181bdaf2dfab41acb455b4ae5b1cc585fe4
Javier <javier.mac13@gmail.com>
2017-03-29 21:02:48 +0000

    ex2

--- app/views.py
+++ app/views.py
@@ -20,6 +20,31 @@ def home():
     """Render website's home page."""
     return render_template('home.html')
 
+@app.route('/api/thumbnails', methods=["GET"])
+def thumber():
+    url = "http://www.espnfc.com/club/chelsea/363/index"
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    
+    # This will look for a meta tag with the og:image property
+    og_image = (soup.find('meta', property='og:image') or
+                        soup.find('meta', attrs={'name': 'og:image'}))
+    
+    if og_image and og_image['content']:
+        print og_image['content']
+
+    # This will look for a link tag with a rel attribute set to 'image_src'
+    thumbnail_spec = soup.find('link', rel='image_src')
+    if thumbnail_spec and thumbnail_spec['href']:
+        print thumbnail_spec['href']
+        print ''
+    
+    images = []
+    image = """"%s"""
+    for img in soup.findAll("img", src=True):
+        collect= (urlparse.urljoin(url, img["src"]))
+        images+=[collect]
+    return jsonify(error= None, message ="Success",thumbnails=images)
 
 ###
 # The functions below should be applicable to all Flask apps.
